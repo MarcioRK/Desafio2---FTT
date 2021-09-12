16ec9d8a491fef7799230846e1f8ebb94a8748b3
MARCIO RIOS KUNIMURA <marcio_rios_kunimura@hotmail.com>
2021-09-11 22:36:05 -0300

    Comments

--- src/App.java
+++ src/App.java
@@ -4,52 +4,48 @@ import java.util.*;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        File inputFile = new File("/Users/MarcioRK/Documents/Codigos_Faculdade/Desafio2-FTT/src/TestInput.txt");
-        /*Scanner sc = new Scanner(inputFile);
-        Queue < String > lines = new LinkedList < > ();
+        File inputFile = new File("C:/Users/MarcioRK/Documents/Codigos_Faculdade/Desafio2---FTT/src/ImputTest.txt");
+        //String path = args[0];
 
-        while (sc.hasNextLine()) {
-            String line = sc.nextLine();
-            lines.add(line);
-        }*/
+        //System.out.println(path);
 
         //Creates the output file
-        File outputFile = new File("TestOutput.txt");
+        File outputFile = new File("Output.txt");
         outputFile.createNewFile();
-        FileWriter fw = new FileWriter("TestOutput.txt");
+        FileWriter fw = new FileWriter("Output.txt");
 
+        //Read the file
         BufferedReader br = new BufferedReader(new FileReader(inputFile));
+        //BufferedReader br = new BufferedReader(new FileReader(path));
+
+        //Validade the string
         try {
             String line;
             while ((line = br.readLine()) != null) {
                 char character;
                 char top = ' ';
-                Boolean isValid = true;
                 Stack < Character > stackChar = new Stack < > ();
 
+                //Repeat for each character in string
                 for (Integer i = 0; i < line.length(); i++) {
                     character = line.charAt(i);
-
-                    if (character == '{' || character == '(' || character == '[' || character == '<') {
+                    
+                    if (character == '{' || character == '(' || character == '[' || character == '<') {             //Opening
                         stackChar.push(character);
-                    } else if ((character == '}' || character == ')' || character == ']' || character == '>')) {
+                    } else if ((character == '}' || character == ')' || character == ']' || character == '>')) {    //Closing
+
+                        //Get the character from the top of the stack
                         top = stackChar.peek();
 
+                        //Really closing
                         if ((character == '}' && top == '{') || (character == ')' && top == '(') || (character == ']' && top == '[') || (character == '>' && top == '<')) {
                             stackChar.pop();
-                        } else {
-                            isValid = false;
                         }
                     }
                 }
    
-                if (stackChar.isEmpty()) {
-                    isValid = true;
-                } else {
-                    isValid = false;
-                }
-
-                if (isValid) {
+                //If stackChar is empty and top isn't ' ', it hasn't passed throught "opening if"
+                if (stackChar.isEmpty() && top != ' ') {
                     line += " - Ok";
                 } else {
                     line += " - Error";
@@ -59,7 +55,6 @@ public class App {
 
                 fw.write(line);
                 fw.write(System.getProperty("line.separator"));
-
             }
         } finally {
             br.close();
@@ -67,9 +62,6 @@ public class App {
 
         fw.close();
 
-        //sc.close();
-
-        //String outputText = validadeLines(lines);
-        //validadeLines(lines);
+        System.out.println("\nOutput created\nOutput file name: Output.txt\n");
     }
 }
